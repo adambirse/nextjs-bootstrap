4b2ce6b36660ae24ada932dfd7991820fdbdf203
Adam Birse <adambirse@gmail.com>
2022-03-01 14:47:53 +0000

    Added tailwind.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 export default function Page() {
   return (
-    <>
-      <h1>My NextJS bootstrap project</h1>
+    <div className="flex flex-col space-y-10">
+      <div className="flex justify-center">
+        <h1 className="text-3xl font-bold underline">
+          My NextJS bootstrap project
+        </h1>
+      </div>
       <p>A starter for 10 project to bootstrap nextJS development</p>
-    </>
+    </div>
   );
 }
